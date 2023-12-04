50a105ef536079cd11239a56d3dafb8165935706
jfmartinsvred1 <jfmartinsvred@gmail.com>
2023-12-04 17:49:37 -0300

    Envio De Email funcionando

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using AutenticacaoComEmail.Data;
 using AutenticacaoComEmail.Data.Dtos;
+using AutenticacaoComEmail.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AutenticacaoComEmail.Controllers
@@ -19,6 +20,8 @@ namespace AutenticacaoComEmail.Controllers
         public async Task<IActionResult> Include(LoginUserDto dto)
         {
             await _userDao.IncluirAsync(dto);
+            var outlook = new Email("smtp.office365.com", "TestApiJfmartins@outlook.com", "Apiemail123!");
+            outlook.SendEmail("jfmartinsvred1@gmail.com", "Confirmação De Email");
             return Ok("Criado Com Sucesso");
         }
     }
--- Services/Email.cs
+++ Services/Email.cs
@@ -1,6 +1,61 @@
-﻿namespace AutenticacaoComEmail.Services
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace AutenticacaoComEmail.Services
 {
     public class Email
     {
+        public Email(string provedor, string userName, string password)
+        {
+            Provedor = provedor;
+            UserName = userName;
+            Password = password;
+        }
+
+        public string Provedor { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public void SendEmail(string emailsTo, string subject)
+        {
+            var message = PreparandoEmail(emailsTo, subject);
+            SendEmailBtSmtp(message);
+        }
+
+        private MailMessage PreparandoEmail(string emailsTo, string subject)
+        {
+            var mail = new MailMessage();
+
+            mail.From = new MailAddress(UserName);
+
+            mail.To.Add(emailsTo);
+
+            mail.Subject = subject;
+            mail.Body = generateCode();
+            mail.IsBodyHtml=true;
+
+            return mail;
+        }
+
+        private string generateCode()
+        {
+            string code = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 10);
+
+            return code;
+        }
+
+        private void SendEmailBtSmtp(MailMessage mail)
+        {
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Host = Provedor;
+            smtpClient.Port = 587;
+            smtpClient.EnableSsl = true;
+            smtpClient.Timeout = 5000;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(UserName, Password);
+            smtpClient.Send(mail);
+            smtpClient.Dispose();
+        }
+        
     }
 }
